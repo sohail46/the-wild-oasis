8e33e485052d0246b0f4275aee17a1d30f11b369
Sohail Shaikh <sohail28195@gmail.com>
2025-08-31 12:39:41 +0530

    styled components app init with p0 styles and react routing :tada:

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.3.1' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'off',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+  },
+};
