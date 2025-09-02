971842362564c27473d77c4778822e1c385bc6e8
Sohail Shaikh <sohail28195@gmail.com>
2025-09-02 11:13:55 +0530

    supabase added :zap:

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,23 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((cabins) => {
+      console.log(cabins);
+    });
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://bdaipygcvbmslaupcggt.supabase.co/storage/v1/object/public/cabin-images/cabin-008.jpg"
+        alt="cabin"
+      />
     </Row>
   );
 }
