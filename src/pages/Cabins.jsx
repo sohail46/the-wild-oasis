1715fb603ad3f7b31dd6bc69f5f147aff0da201c
Sohail Shaikh <sohail28195@gmail.com>
2025-09-03 16:28:28 +0530

    rect query init :sparkles:

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,19 @@
-import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((cabins) => {
-      console.log(cabins);
-    });
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://bdaipygcvbmslaupcggt.supabase.co/storage/v1/object/public/cabin-images/cabin-008.jpg"
-        alt="cabin"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
