935fe7fddfba1ab707274b2a05fde666c557a32e
Sohail Shaikh <sohail28195@gmail.com>
2025-09-23 12:37:59 +0530

    compund component pattern added :hammer:

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import FormRow from '../../ui/FormRow';
 import { useCreateCabin } from './useCreateCabin';
 import { useEditCabin } from './useEditCabin';
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { id: editId, ...editValues } = cabinToEdit;
   const isEditSession = Boolean(editId);
 
@@ -28,13 +28,21 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     if (isEditSession) {
       editCabin(
         { newCabinData: { ...data, image }, id: editId },
-        { onSuccess: () => reset() }
+        {
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
+        }
       );
     } else {
       createCabin(
         { ...data, image },
         {
-          onSuccess: () => reset(),
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
         }
       );
     }
@@ -46,7 +54,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? 'modal' : 'regular'}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -120,7 +131,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,9 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
 import CabinTable from '../features/cabins/CabinTable';
-import Button from '../ui/Button';
-import { useState } from 'react';
-import CreateCabinForm from '../features/cabins/CreateCabinForm';
+import AddCabin from '../features/cabins/AddCabin';
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -17,16 +12,8 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
-
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new cabin
-        </Button>
+        <AddCabin />
       </Row>
-      {showForm && (
-        <Row>
-          <CreateCabinForm onClose={() => setShowForm(false)} />
-        </Row>
-      )}
     </>
   );
 }
