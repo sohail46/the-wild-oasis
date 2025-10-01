3eaac3aabbdbf0ee974fe975cfd0a6c788487817
Sohail Shaikh <sohail28195@gmail.com>
2025-10-01 12:37:11 +0530

    finishing reusable components :zap:

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -88,7 +88,11 @@ function Row({ children }) {
   );
 }
 
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data show at the moment.</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Body = Body;
