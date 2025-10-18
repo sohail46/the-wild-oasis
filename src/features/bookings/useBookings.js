bd1f64939d317310821a0351c7ea8ef12b6b5e2f
Sohail Shaikh <sohail28195@gmail.com>
2025-10-18 18:51:24 +0530

    booking table with filter, sort and pagination :sparkles:

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
+
+  const {
+    isLoading,
+    data: { data: bookings, count } = {},
+    error,
+  } = useQuery({
+    queryKey: ['bookings', filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
+  });
+
+  return { isLoading, bookings, error, count };
+}
