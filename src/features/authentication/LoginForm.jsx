76e764d2d4f8cf7582a082af6481fcfde7f19ac2
Sohail Shaikh <sohail28195@gmail.com>
2025-10-21 12:54:22 +0530

    signup completed :zap:

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from './usLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('sohail@example.com');
-  const [password, setPassword] = useState('pass123');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,17 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { signup as signUpApi } from '../../services/apiAuth';
+import toast from 'react-hot-toast';
+
+export function useSignup() {
+  const { mutate: signup, isPending: isLoading } = useMutation({
+    mutationFn: signUpApi,
+    onSuccess: (user) => {
+      toast.success(
+        "Account successfully created! Please verify the new account from the user's email address"
+      );
+    },
+  });
+
+  return { signup, isLoading };
+}
