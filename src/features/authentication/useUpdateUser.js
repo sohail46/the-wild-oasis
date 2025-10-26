d35a52e1c11dc904439f95ce7ed03b69724c8be3
Sohail Shaikh <sohail28195@gmail.com>
2025-10-26 10:56:55 +0530

    fixing :bug: for updating user image :zap:

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -10,8 +10,8 @@ export function useUpdateUser() {
     mutationFn: (userData) => updateCurrentUser(userData),
     onSuccess: ({ user }) => {
       toast.success('User account successfully updated!');
-      // queryClient.setQueryData('user', user);
-      queryClient.invalidateQueries({ queryKey: ['user'] });
+      queryClient.setQueryData(['user'], user);
+      // queryClient.invalidateQueries({ queryKey: ['user'] });
     },
     onError: (err) => toast.error(err.message),
   });
